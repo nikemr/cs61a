bafd09ec8af2c1c8b820a78b1b0555ff614160e5
nikemr <nkemer@gmail.com>
2020-09-20 17:24:30 +0300

    p5 working

--- hog/hog.py
+++ hog/hog.py
@@ -68,10 +68,32 @@ def free_bacon(score):
 #     smaller_digit=min(tens_digit,ones_digit)
 #     return smaller_digit+1
 
+def take_turn(num_rolls, opponent_score, dice=six_sided):
+    """Simulate a turn rolling NUM_ROLLS dice, which may be 0 (Free Bacon).
+    Return the points scored for the turn by the current player.
+
+    num_rolls:       The number of dice rolls that will be made.
+    opponent_score:  The total score of the opponent.
+    dice:            A function that simulates a single dice roll outcome.
+    """
+    # Leave these assert statements here; they help check for errors.
+    assert type(num_rolls) == int, 'num_rolls must be an integer.'
+    assert num_rolls >= 0, 'Cannot roll a negative number of dice in take_turn.'
+    assert num_rolls <= 10, 'Cannot roll more than 10 dice.'
+    assert opponent_score < 100, 'The game should be over.'
+    # BEGIN PROBLEM 3
+    if not num_rolls:
+        score= free_bacon(opponent_score)
+    else:
+        score= roll_dice(num_rolls,dice)
+    return score    
+    "*** YOUR CODE HERE ***"
+    # END PROBLEM 3
+
+
 # def take_turn(num_rolls, opponent_score, dice=six_sided):
 #     """Simulate a turn rolling NUM_ROLLS dice, which may be 0 (Free Bacon).
 #     Return the points scored for the turn by the current player.
-
 #     num_rolls:       The number of dice rolls that will be made.
 #     opponent_score:  The total score of the opponent.
 #     dice:            A function that simulates a single dice roll outcome.
@@ -82,28 +104,10 @@ def free_bacon(score):
 #     assert num_rolls <= 10, 'Cannot roll more than 10 dice.'
 #     assert opponent_score < 100, 'The game should be over.'
 #     # BEGIN PROBLEM 3
-#     if not num_rolls:
-#         score= free_bacon(opponent_score)
-#     else:
-#         score= roll_dice(num_rolls)
-#     return score    
 #     "*** YOUR CODE HERE ***"
+#     return free_bacon(opponent_score) if num_rolls is 0 else roll_dice(num_rolls, dice)
 #     # END PROBLEM 3
-def take_turn(num_rolls, opponent_score, dice=six_sided):
-    """Simulate a turn rolling NUM_ROLLS dice, which may be 0 (Free bacon).
-    num_rolls:       The number of dice rolls that will be made.
-    opponent_score:  The total score of the opponent.
-    dice:            A function of no args that returns an integer outcome.
-    """
-    assert type(num_rolls) == int, 'num_rolls must be an integer.'
-    assert num_rolls >= 0, 'Cannot roll a negative number of dice.'
-    assert num_rolls <= 10, 'Cannot roll more than 10 dice.'
-    assert opponent_score < 100, 'The game should be over.'
 
-    if num_rolls == 0:
-        return 1 + max(opponent_score % 10, opponent_score // 10)
-    else:
-        return roll_dice(num_rolls, dice)
 
 def extra_turn(player_score, opponent_score):
     """Return whether the player gets an extra turn."""
