2b5882467eaef30d8bf571073cd766ce7e5dcbf7
nikemr <nkemer@gmail.com>
2020-11-15 21:37:34 +0300

    Lab 8 done

--- lab08/lab08.py
+++ lab08/lab08.py
@@ -44,20 +44,6 @@ def every_other(s):
             return kept
 
 
-
-def keep_if_link( s):
-    """Return a list with elements of s for which f(e) is true."""
-    
-    if s == ():
-        return ()
-    else:
-        kept = keep_if_link(s.rest)
-        if s.first%2==0:
-            return s.first, kept
-        else:
-            return kept
-
-
 def cumulative_mul(t):
     """Mutates t so that each node's label becomes the product of all labels in
     the corresponding subtree rooted at t.
