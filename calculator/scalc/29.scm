7fbc32da342f41b277fd6555596985858b91b7d2
nikemr <nkemer@gmail.com>
2020-11-27 23:48:39 +0300

    some call exps

---
+++ calculator/scalc/29.scm
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8"/>
+    <title>61A Code</title>
+	  <script> var initData = {"loadFile": {"data": "(* 1 2 3)\n(+)\n(+ 2 (/ 4 8))\n(+ 2\n     (/ 4 (+ \n   1 1 1 1 1 1 1 1)\n) )\n\n2\n(2)\n(+)\n(+ 1 2 3)\n(-)\n(- 2)\n(- 10 1 2 3)\n(* 1 2 3 4)\n(/)\n(/ 4)\n(/ 1024 2 2 2 2 2)\n(? 2 3)\n\n2.3.4\n)\n()\n(-)\n(/ 1 0)\n\n", "fileName": "29.scm", "shareRef": null}} </script>
+  <link rel="shortcut icon" href="/favicon.ico"><link href="/static/pace/pace.css" rel="stylesheet"></head>
+  <body>
+  <script type="text/javascript" src="/static/pace/pace.min.js"></script><script type="text/javascript" src="/static/main.js"></script></body>
+</html>
